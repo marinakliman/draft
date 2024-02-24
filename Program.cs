b98424bf11b85d071d3c836300f19700ac1e16a7
Marina Kliman <marinakliman9@gmail.com>
2024-02-24 12:04:33 +1000

    записали первую часть решения

--- Program.cs
+++ Program.cs
@@ -2,3 +2,23 @@
 // длина которых меньше или равна 3 символам. Первоначальный массив можно ввести с клавиатуры ,
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется использовать коллекции ,
 // лучше обойтись исключительно массивами.
+
+Console.WriteLine("Введите строки через запятую: ");
+string input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("Вы не ввели строки");
+}
+else 
+{
+    string[] originalArray = input.Split(',');
+    string[] newArray = FilterStrings(originalArray);
+
+    Console.WriteLine("Новый массив строк: ");
+    foreach (Version item in newArray)
+    {
+        Console.WriteLine(item);
+    }
+}
+
